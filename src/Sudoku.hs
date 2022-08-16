3d65332e1c42248c1fa267f6be95d7ac5e1be3d3
chrisgrounds <iamchrisgrounds@gmail.com>
2022-08-16 15:21:22 +0100

    Solve challenge for sample1

---
+++ src/Sudoku.hs
@@ -0,0 +1,29 @@
+module Sudoku (SudokuBoard(..), isValidSolution) where
+
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
+
+newtype SudokuBoard = SudokuBoard {
+    get :: [[Int]]
+} deriving (Show, Eq)
+
+isValidSolution :: SudokuBoard -> Bool
+isValidSolution board =
+  let rowsAreValid     = all axisIsValid $ get board
+      colsAreValid     = all axisIsValid $ transpose (get board)
+      subGridsAreValid = all axisIsValid $ constructSubGrids (get board)
+   in rowsAreValid && colsAreValid && subGridsAreValid
+
+axisIsValid :: [Int] -> Bool
+axisIsValid xs = length xs == 9 && unique xs
+
+constructSubGrids :: [[Int]] -> [[Int]]
+constructSubGrids = concatMap constructSubGrid . constructSubGrids'
+  where constructSubGrids' :: [[Int]] -> [[[Int]]]
+        constructSubGrids' = map (take 3 . drop 3) . chunksOf 3
+        constructSubGrid :: [[Int]] -> [[Int]]
+        constructSubGrid = concatMap (take 3 . drop 3) . chunksOf 3
+
+unique :: (Eq a) => [a] -> Bool
+unique []     = True
+unique (x:xs) = x `notElem` xs && unique xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.Hspec
+import Sudoku
+
+sample1 :: SudokuBoard
+sample1 = SudokuBoard [
+    [1, 5, 2, 4, 8, 9, 3, 7, 6],
+    [7, 3, 9, 2, 5, 6, 8, 4, 1],
+    [4, 6, 8, 3, 7, 1, 2, 9, 5],
+    [3, 8, 7, 1, 2, 4, 6, 5, 9],
+    [5, 9, 1, 7, 6, 3, 4, 2, 8],
+    [2, 4, 6, 8, 9, 5, 7, 1, 3],
+    [9, 1, 4, 6, 3, 7, 5, 8, 2],
+    [6, 2, 5, 9, 4, 8, 1, 3, 7],
+    [8, 7, 3, 5, 1, 2, 9, 6, 4]
+  ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Sudoku" $ do
+    it "sample1 is valid" $ do
+      isValidSolution sample1 `shouldBe` True
