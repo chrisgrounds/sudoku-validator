0e898468c5dbe63ea32b92d30a493eebc03aff9c
chrisgrounds <iamchrisgrounds@gmail.com>
2022-08-16 15:23:08 +0100

    Use Sudoku module in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import Sudoku (SudokuBoard(..), isValidSolution)
+
+sample1 :: SudokuBoard
+sample1 = SudokuBoard [
+    [1, 5, 2, 4, 8, 9, 3, 7, 6],
+    [7, 3, 9, 2, 5, 6, 8, 4, 1],
+    [4, 6, 8, 3, 7, 1, 2, 9, 5],
+    [3, 8, 7, 1, 2, 4, 6, 5, 9],
+    [5, 9, 1, 7, 6, 3, 4, 2, 8],
+    [2, 4, 6, 8, 9, 5, 7, 1, 3],
+    [9, 1, 4, 6, 3, 7, 5, 8, 2],
+    [6, 2, 5, 9, 4, 8, 1, 3, 7],
+    [8, 7, 3, 5, 1, 2, 9, 6, 4]
+  ]
 
 main :: IO ()
-main = someFunc
+main = print $ isValidSolution sample1
